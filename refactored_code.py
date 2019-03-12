2b6e5aab2a80a62339b1e6aa104b6266c9cae491
V <vilma.daukantaite@gmail.com>
2019-03-12 16:36:20 +0100

    trying to make it shorter - making it worse

--- refactored_code.py
+++ refactored_code.py
@@ -1,33 +1,16 @@
 import alphabet
 import encryption
 
-
-choose_action=0
+alph = alphabet.retrieve_alphabet()
 key = alphabet.shuffle_alphabet()
+choose_action = 0
 while choose_action != 3:
     choose_action = input("Type 1 for encrypting a message. \nType 2 for decripting a message. \nType 3 to exit the program ")
     if choose_action == "1":
-        choose_encryption = input("Type 1 for Caesarean cypher. \nType 2 for Translation table ")
-        message = input("What is your message ")
-        if choose_encryption == "1":
-            key_caesar = alphabet.shuffle_caesarian()
-            encryption.encrypt_decrypt(message, alphabet, key_caesar)
-        elif choose_encryption == "2":
-            encryption.encrypt_decrypt(message, alphabet, key)
-        else:
-            print("Only two methods available: 1 or 2")
+        encryption.encrypt_message()
     elif choose_action == "2":
-        choose_encryption = input("Type 1 for Caesarean cypher. \nType 2 for Translation table ")
-        message = input("What is your message ")
-        if choose_encryption == "1":
-            key_caesar = alphabet.shuffle_caesarian()
-            encryption.encrypt_decrypt(message, key_caesar, alphabet)
-        elif choose_encryption == "2":
-            encryption.encrypt_decrypt(message, key, alphabet)
-        else:
-            print("Only two methods available: 1 or 2")
+        encryption.decrypt_message()
     elif choose_action == "3":
         break
     else:
         print("Options are only 1, 2 or 3")
-
