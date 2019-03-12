856a437eb30d590d51de9e534786ba71ed1f7930
V <vilma.daukantaite@gmail.com>
2019-03-12 14:12:34 +0100

    Adding description to functions

--- alphabet.py
+++ alphabet.py
@@ -3,6 +3,10 @@ import string
 from collections import deque
 
 def retrieve_alphabet():
+    '''
+
+    :return:
+    '''
     alphabet = string.printable.split(' ')[0]
     return alphabet
 
@@ -10,6 +14,10 @@ def retrieve_alphabet():
 # if you provide a list of characters to be used
 
 def shuffle_alphabet():
+    '''
+
+    :return:
+    '''
     key_to_shuffle = list(retrieve_alphabet())
     random.shuffle(key_to_shuffle)
     key = ''.join(key_to_shuffle)
@@ -17,15 +25,24 @@ def shuffle_alphabet():
 
 
 def getKey():
+    '''
+
+    :return:
+    '''
     key = 0
     while True:
         print(f'What number do you want to use for the cypher between -{len(retrieve_alphabet())} and {len(retrieve_alphabet())}')
-        key_c = int(input())
-        if key_c >= (-1 * len(retrieve_alphabet())) and key_c <= len(retrieve_alphabet()):
-            return key_c
+        key_provided = int(input())
+        if key_provided >= (-1 * len(retrieve_alphabet())) and key_provided <= len(retrieve_alphabet()):
+            return key_provided
 
 # Function to create Caesarian cypher translation table using the key
-def shuffle_caesarian(base = alphabet):
+def shuffle_caesarian():
+    '''
+    Function takes alphabet and shuffles it to the right or to the left by the user
+    provided key and returns a shuffled alphabet
+    :return: str
+    '''
     d = deque(retrieve_alphabet())
     d.rotate(getKey())
     key_caesar = ''.join(list(d))
