b362e1744e2d0be0e1e2ff337baa42bdd1c4f775
alvaro <alvarofragosoc@hotmail.com>
2019-03-12 17:10:15 +0100

    Adding comments to the functions

--- enigma_refact.py
+++ enigma_refact.py
@@ -1,33 +1,35 @@
 import  my_library as my
 
-# Actual program
 
 def enigma():
     """
 
-    :return:
+    :return: This function returns the message encrypted or decrypted using the functions from "my_library"
     """
     selection = input("Do you want to encrypt or decrypt? E/D: ")
+    #Runs if user selects E (Encrypt)
     if selection == 'E' or selection == 'e':
-        res_rot1 = my.rot1()
-        res_rot2 = my.rot2()
-        res_rot3 = my.rot3()
+        encrypt_rot1 = my.rot1()
+        encrypt_rot2 = my.rot2()
+        enrypt_rot3 = my.rot3()
         message = my.text()
-        loop1 = my.crypto(message, res_rot1)
-        loop2 = my.crypto(loop1, res_rot2 )
-        loop3 = my.crypto(loop2, res_rot3)
-        loop4 = my.crypto(loop3, res_rot2)
-        print("Your encrypted message is:",my.crypto(loop4,res_rot1))
+        loop1 = my.crypto(message, encrypt_rot1)
+        loop2 = my.crypto(loop1, encrypt_rot2 )
+        loop3 = my.crypto(loop2, enrypt_rot3)
+        loop4 = my.crypto(loop3, encrypt_rot2)
+        print("Your encrypted message is:",my.crypto(loop4,encrypt_rot1))
+    #Runs if user selects D (Decrypt)
     elif selection == 'D' or selection == 'd':
-            in_res_rot1 = my.in_rot1()
-            in_res_rot2 = my.in_rot2()
-            in_res_rot3 = my.in_rot3()
-            in_message = my.in_text()
-            in_loop1 = my.crypto(in_message, in_res_rot1)
-            in_loop2 = my.crypto(in_loop1, in_res_rot2 )
-            in_loop3 = my.crypto(in_loop2, in_res_rot3)
-            in_loop4 = my.crypto(in_loop3, in_res_rot2)
-            print("Your decrypted message is:",my.crypto(in_loop4,in_res_rot1))
+        decrypt_rot1 = my.in_rot1()
+        decrypt_rot2 = my.in_rot2()
+        decrypt_rot3 = my.in_rot3()
+        de_message = my.in_text()
+        de_loop1 = my.crypto(de_message, decrypt_rot1)
+        de_loop2 = my.crypto(de_loop1, decrypt_rot2 )
+        de_loop3 = my.crypto(de_loop2, decrypt_rot3)
+        de_loop4 = my.crypto(de_loop3, decrypt_rot2)
+        print("Your decrypted message is:",my.crypto(de_loop4,decrypt_rot1))
+    #Runs if user types other thing than E or D
     else:
         print(f"You tiped {selection}, please type E/D")
         enigma()
