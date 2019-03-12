050aea777af20218caf408e758aaa1db68be4290
V <vilma.daukantaite@gmail.com>
2019-03-12 14:13:21 +0100

    Adding descriptions to the function

--- encryption.py
+++ encryption.py
@@ -1,6 +1,15 @@
+import string
+
 # Function takes 3 variables to encrypt or decrypt the message
 
 def encrypt_decrypt(m, a, k):
+    '''
+
+    :param m: str
+    :param a: str
+    :param k: str
+    :return:
+    '''
     translation_table = m.maketrans(a, k)
     encrypt_decrypt_message = m.translate(translation_table)
     print(encrypt_decrypt_message)
