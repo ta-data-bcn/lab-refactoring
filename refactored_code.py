0f2a8a66117eef71944293007091fc8881f3c570
V <vilma.daukantaite@gmail.com>
2019-03-12 14:45:10 +0100

    adding library names to make code work

--- refactored_code.py
+++ refactored_code.py
@@ -3,27 +3,27 @@ import encryption
 
 
 choose_action=0
-key = shuffle_alphabet()
+key = alphabet.shuffle_alphabet()
 while choose_action != 3:
     choose_action = input("Type 1 for encrypting a message. \nType 2 for decripting a message. \nType 3 to exit the program ")
     if choose_action == "1":
         choose_encryption = input("Type 1 for Caesarean cypher. \nType 2 for Translation table ")
         message = input("What is your message ")
         if choose_encryption == "1":
-            key_caesar = shuffle_caesarian()
-            encrypt_decrypt(message, alphabet, key_caesar)
+            key_caesar = alphabet.shuffle_caesarian()
+            encryption.encrypt_decrypt(message, alphabet, key_caesar)
         elif choose_encryption == "2":
-            encrypt_decrypt(message, alphabet, key)
+            encryption.encrypt_decrypt(message, alphabet, key)
         else:
             print("Only two methods available: 1 or 2")
     elif choose_action == "2":
         choose_encryption = input("Type 1 for Caesarean cypher. \nType 2 for Translation table ")
         message = input("What is your message ")
         if choose_encryption == "1":
-            key_caesar = shuffle_caesarian()
-            encrypt_decrypt(message, key_caesar, alphabet)
+            key_caesar = alphabet.shuffle_caesarian()
+            encryption.encrypt_decrypt(message, key_caesar, alphabet)
         elif choose_encryption == "2":
-            encrypt_decrypt(message, key, alphabet)
+            encryption.encrypt_decrypt(message, key, alphabet)
         else:
             print("Only two methods available: 1 or 2")
     elif choose_action == "3":
