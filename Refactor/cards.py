7138337dd267960d6aaab9bfa23ecff7053e2c63
Nacho Martinez <nacho.080@gmail.com>
2019-03-12 19:23:33 +0100

    Made visual changes in the cards library to follow PIP8 convention

--- Refactor/cards.py
+++ Refactor/cards.py
@@ -14,9 +14,11 @@ def place_bet():
     else:
         print('You introduced a non valid character. Please introduce a number')
 
+
 # Deal the first two cards.
 def deal_first_hand():
     return [random.choice(useful_deck) for i in range(2)]
+
 
 # Deal an additional card.
 def deal_card():
@@ -29,6 +31,7 @@ def parse_ace_value(hand, value):
         if hand[i] == 'A':
             hand[i] = value
 
+
 # Check natural blackjack, we parse Ace to 11 automatically.
 def natural_blackjack(hand):
     if check_score(hand) == 21:
@@ -36,14 +39,17 @@ def natural_blackjack(hand):
         parse_face_cards(hand)
     return sum(hand[:2]) == 21
 
+
 # Check if player has two cards of the same denomination.
 def check_denomination(hand):
     return hand[0] == hand[1]
+
 
 # Check if player's cards sum 9, 10 or 11
 def check_double_down(hand):
     result = sum(hand[:2])
     return result == 9 or result == 10 or result == 11
+
 
 # Sum the value of the cards
 def check_score(hand):
@@ -61,22 +67,19 @@ def compare_hands(p_hand, d_hand, player_wins, dealer_wins, tie):
 
     if p_score > 21:
         print("You bust with %d points. Dealer wins" % p_score)
-        dealer_wins = True
+
     if d_score > 21:
         print("Dealer busts with %d points. Players wins" % d_score)
-        player_wins = True
+
     if p_score == d_score:
         print("There has been a tie. Player points: %d and hand: %s. Dealer points: %d and hand: %s" % (
             p_score, p_hand, d_score, d_hand))
-        tie = True
 
     if p_score > d_score:
         print("Player wins with %d points with the hand %s" % (p_score, p_hand))
-        player_wins = True
 
     if p_score < d_score:
         print("Dealer wins with %d points with the hand %s" % (d_score, d_hand))
-        dealer_wins = True
 
 
 # Check if the player has less than 21 points, to give him a new card.
@@ -93,6 +96,7 @@ def hit(hand):
 
 # Change the value of the face cards, minus the ace, to its point value.
 def parse_face_cards(hand):
+
     for i in range(len(hand)):
         if hand[i] == 'K':
             hand[i] = face_cards['K']
@@ -103,8 +107,10 @@ def parse_face_cards(hand):
         if hand[i] == 'J':
             hand[i] = face_cards['J']
 
+
 def dealer_play(hand):
-    while(check_score(hand) <= 14):
+
+    while check_score(hand) <= 14:
         hit(hand)
     print("Dealer's hand: ", hand)
     return hand
