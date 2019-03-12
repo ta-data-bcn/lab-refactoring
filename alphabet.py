814928281ef2a9b8e750c44bf9e8bed84607d0fa
V <vilma.daukantaite@gmail.com>
2019-03-12 13:54:55 +0100

    Incorporated retrieve_alphabet function into shuffle and getKey functions

--- alphabet.py
+++ alphabet.py
@@ -2,23 +2,31 @@ import random
 import string
 from collections import deque
 
-# getting the cleaned ascii
-alph = string.printable
-alphabet_split = alph.split(" ")
-alphabet = alphabet_split[0]
+def retrieve_alphabet():
+    alphabet = string.printable.split(' ')[0]
+    return alphabet
 
 # Function that allows to create your own key with any characters that a language might use
 # if you provide a list of characters to be used
 
-def shuffle_alphabet(base = alphabet):
-    key_to_shuffle = list(base)
+def shuffle_alphabet():
+    key_to_shuffle = list(retrieve_alphabet())
     random.shuffle(key_to_shuffle)
     key = ''.join(key_to_shuffle)
     return key
 
+
+def getKey():
+    key = 0
+    while True:
+        print(f'What number do you want to use for the cypher between -{len(retrieve_alphabet())} and {len(retrieve_alphabet())}')
+        key_c = int(input())
+        if key_c >= (-1 * len(retrieve_alphabet())) and key_c <= len(retrieve_alphabet()):
+            return key_c
+
 # Function to create Caesarian cypher translation table using the key
-def shuffle_caesarian(base=alphabet):
-    d = deque(alphabet)
+def shuffle_caesarian(base = alphabet):
+    d = deque(retrieve_alphabet())
     d.rotate(getKey())
     key_caesar = ''.join(list(d))
     return key_caesar
