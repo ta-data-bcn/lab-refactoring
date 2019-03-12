986f062cd12263cf506e58c07a18042b708c056b
Adrià <adria.ferrer.abad@gmail.com>
2019-03-12 14:07:02 +0100

    deck_values library documentation updated

--- deck_values.py
+++ deck_values.py
@@ -2,7 +2,7 @@ def deck_values(a_as_one):
     """
     Define the values for each card of the deck depending on the desired value for the ace cards.
     :param a_as_one: boolean, =True if we want the ace to count as one, =False if we want the ace to count 11
-    :return:
+    :return: Dictionary with keys = cards (strings) and values = values of each card (int)
     """
     values_of_the_deck = {}
     hearts = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
