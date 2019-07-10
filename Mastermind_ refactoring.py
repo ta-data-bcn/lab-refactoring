b571de828fcfd7a32edc13c13924656c6de31ea9
Laurent Guerguy <laurent_guerguy@hotmail.com>
2019-07-10 13:56:46 +0200

    Code Refactoring done using Library

--- Mastermind_ refactoring.py
+++ Mastermind_ refactoring.py
@@ -2,14 +2,8 @@
 
 # Import libraries to be used in the code below
 import random
-# Define a function to ask the player to choose 4 colours between blue, green, orange, pink, red, yellow
-import ask_player
-## Define a random generator function to generate the computer choice of colours and positions
-import random_colour
-## Define a function to give the result of a round:
-### Black means the right colour in the right position
-### White means this colour is present in the computer choice but in the wrong position
-import give_result
+# Import all the functions used in the program by import the mastermind_functions library
+import mastermind_functions
 
 
 # Set the variables respectively for the choice of the computer, the choice of the player, the number of rounds
@@ -20,24 +14,24 @@ round_number = 10
 colours = ["blue", "green", "orange", "pink", "red", "yellow"]
 
 
-# Define the choice colours of the computer
+# Define the colours chosen by the computer
 
 ## The result of randomColour is a list that I transform to a tuple called computer_tuple so it stays unchanged as a
 ## reference for the initial computer choice of colours.
 ## NOTE: The computer variable is modified during the giveResult function (for reasons I don't have time to solve
 ## now). That is why I need to create and use computer_tuple.
-computer_tuple = tuple(random_colour())
+computer_tuple = tuple(mastermind_functions.random_colour(colours))
 
 
 # Define the choice of the computer
-computer = random_colour()
+# computer = mastermind_functions.random_colour()
 
 # Play the game. The game is composed of a maximum of 10 rounds. if after 10 rounds the user has not found the
 # computer's choice then the computer wins
 for i in range(10):
     print(f"This is round number {i + 1}")
-    ask_player()
-    result2 = give_result()
+    mastermind_functions.ask_player(computer_tuple, player, colours)
+    result2 = mastermind_functions.give_result(computer_tuple, player)
     if result2 == ["black", "black", "black", "black"]:
         print("Congratulations! You won!!")
 
--- function_random_colour.py
+++
@@ -1,5 +0,0 @@
-## Define a random generator function to generate the computer choice of colours and positions
-def random_colour():
-    """ This function generates the computer choice of colours and positions"""
-    a = [random.choice(colours) for i in range(4)]
-    return (a)
