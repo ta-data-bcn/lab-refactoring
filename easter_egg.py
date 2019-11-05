b6a8854a5f73d8fc63c91f982fda202939e02f45
jaumevr15 <53271315+jaumevr15@users.noreply.github.com>
2019-11-05 14:42:31 +0100

    Created easter_egg.py

---
+++ easter_egg.py
@@ -0,0 +1,8 @@
+#EASTER EGG CODE because life is too short for not having Easter eggs telling you how awesome you are.
+def easteregg():
+    easter_egg = {
+"Jaume": "The hottest smartest coolest man on Earth",
+"Labs": "How many left to heaven?",
+"Jaumean": "Also known as the best language in the world"
+}
+    return easter_egg
