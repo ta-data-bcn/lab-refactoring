0c094c068e8eb27a8c6825a33918e23e7da6e707
beto.sibileau <asibileau@gmail.com>
2020-04-19 16:32:34 +0200

    function script that returns the third point to make a wining line

---
+++ my-code/getWiningChoice.py
@@ -0,0 +1,15 @@
+
+# this function takes a potential wining pair of points as input (in two tuples)
+# it returns the corresponding point that makes a wining line in Tic Tac Toe (in a tuple)
+# uses global variable: ticTacDict
+
+def getWiningChoice(tuples2win,gameDict=ticTacDict):
+    (cosTheta,sinTheta) = determine_direction(*tuples2win)
+    
+    for aTuple in gameDict.values():
+        if (aTuple != tuples2win[0]) & (aTuple != tuples2win[1]):
+            (cosGamma,sinGamma) = determine_direction(tuples2win[1],aTuple)
+            
+            if ((cosTheta,sinTheta) == (cosGamma,sinGamma)) | \
+            ((cosTheta,sinTheta) == negative((cosGamma,sinGamma))).all():
+                return aTuple
