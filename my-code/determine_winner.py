a48d3fc2a497c18bc4fd8b893aa15ec773e43fb3
beto.sibileau <asibileau@gmail.com>
2020-04-19 16:37:03 +0200

    function script that checks for three in line or a winner

---
+++ my-code/determine_winner.py
@@ -0,0 +1,18 @@
+
+# this function verifies if there's a winner (three points in a line)
+# uses global variable: ticTacDict
+
+def determine_winner(movesHistory,gameDict=ticTacDict):
+    nMoves = len(movesHistory)
+    if nMoves >= 3:
+        for i in range(nMoves):
+            for j in range(nMoves):
+                if j > i:
+                    (cosTheta,sinTheta) = determine_direction(movesHistory[j],movesHistory[i])
+                for k in range(nMoves):
+                    if (j > i) & (k > j):
+                        (cosGamma,sinGamma) = determine_direction(movesHistory[k],movesHistory[j])
+                        if ((cosTheta,sinTheta) == (cosGamma,sinGamma)) | \
+                        ((cosTheta,sinTheta) == negative((cosGamma,sinGamma))).all():
+                            return True
+    return False
