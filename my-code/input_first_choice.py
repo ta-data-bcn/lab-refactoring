36ee0e45e517666ddc77a32df80709550ddf9777
beto.sibileau <asibileau@gmail.com>
2020-04-19 16:11:23 +0200

    indent modified in input first choice

--- my-code/input_first_choice.py
+++ my-code/input_first_choice.py
@@ -1,6 +1,7 @@
 
 # Input who's begining the game
 # Error handling on typos checked (case insensitive and strip spaces)
+
 def input_first_choice():
     
     firstChoice = input("Who will begin the game: Cpu or You? (Type: cpu or me, case not-sensitive) ")
